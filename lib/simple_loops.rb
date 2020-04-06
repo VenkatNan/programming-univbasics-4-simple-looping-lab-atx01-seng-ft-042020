642cfb87af37eba767de01995e6e93b0bcb8c32c
Nandita Venkat <nanditavenkat@gmail.com>
2020-04-06 15:20:02 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,26 @@
-# Write your methods here
+def loop_message_five_times(string)
+  count=0
+  while count < 5
+    puts string
+    count += 1
+  end
+end
+
+def loop_message_n_times
+  
+end
+
+def output_array (array)
+  count = 0
+  while count < array.length do
+    puts 
+    array[count]
+    count += 1
+  end
+end
+  
+end
+
+def return_string_array
+  
+end
